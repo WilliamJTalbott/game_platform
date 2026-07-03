ac671155e87275636fdf25705d60e8af390874e7
William <William@talbott.ws>
2026-07-03 13:53:43 -0400

    Made 'Rules' restful

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,15 @@ Rails.application.routes.draw do
   resources :passwords, param: :token
 
   root 'games#index'
-  get 'games/history', to: 'games#history'
-  get "stats/index"
-  get "pages/rules"
 
-  resources :pages, only: [:index]
-  resources :stats, only: [:index]
+  resources :users
 
   resources :games do
     resources :players, only: [:create, :show]
   end
 
+  resources :stats, only: [:index]
   resources :history, only: [:index]
-  resources :users
+  resources :rules, only: [:index]
 
 end
